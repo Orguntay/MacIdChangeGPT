d1e7d786e2ae47d93cb53d25e794c60c9630804c
Orguntay <erolaktepe@gmail.com>
2022-12-26 20:46:20 +0300

    Add project files.

---
+++ Busines/Mac.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+
+namespace MacIdChangeGPT.Busines
+{
+    public static class Mac
+    {
+        //        private const string baseReg =
+        //@"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\";
+
+        private const string baseReg =
+@"Bilgisayar\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0011\Ndi\Params\NetworkAddress\"; // 
+
+        public static string ReadMAC()
+        {
+            RegistryKey rkey;
+            string MAC;
+            rkey = Registry.LocalMachine.OpenSubKey("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}\\0011\\", true); //--->this is the string to change // 9, 11, 17
+            //rkey = Registry.LocalMachine.OpenSubKey(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\00017\Ndi\params\NetworkAddress\", true); //--->this is the string to change // 0009, 0011, 0017
+            MAC = (string)rkey.GetValue("NetworkAddress");
+            rkey.Close();
+            return MAC;
+        }
+
+        public static bool SetMAC(string newmac)
+        {
+            bool ret = false;
+
+            RegistryKey rk = Registry.LocalMachine;
+            string registryValue;
+            string keyValue = "NetworkAddress";
+            // Bilgisayar\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0011
+            string keyPath = @"SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0011";
+            RegistryKey subkey = rk.OpenSubKey(keyPath, RegistryKeyPermissionCheck.ReadWriteSubTree, RegistryRights.FullControl);
+            var rgstryValueObj = subkey.GetValue(keyValue);
+            if (rgstryValueObj != null)
+                registryValue = rgstryValueObj.ToString();
+
+            //using (RegistryKey bkey = GetBaseKey())
+            using (RegistryKey key = rk.OpenSubKey(keyPath, true))// + nicid // + RegistryView.Default
+            {
+                if (key != null)
+                {
+                    key.SetValue(keyValue, newmac, RegistryValueKind.String);
+                    rk.SetValue("Enable", 0);
+                    rk.SetValue("Enable", 1);
+                    ret = true;
+
+
+                    //ManagementObjectSearcher mos = new ManagementObjectSearcher(
+                    //    new SelectQuery("SELECT * FROM Win32_NetworkAdapter WHERE Index = " + nicid));
+
+                    //foreach (var o in mos.Get().OfType<ManagementObject>())
+                    //{
+                    //    o.InvokeMethod("Disable", null);
+                    //    o.InvokeMethod("Enable", null);
+                    //    ret = true;
+                    //}
+                }
+            }
+
+            return ret;
+        }
+
+        public static IEnumerable<string> GetNicIds()
+        {
+            using (RegistryKey bkey = GetBaseKey())
+            using (RegistryKey key = bkey.OpenSubKey(baseReg))
+            {
+                if (key != null)
+                {
+                    foreach (string name in key.GetSubKeyNames().Where(n => n != "Properties"))
+                    {
+                        using (RegistryKey sub = key.OpenSubKey(name))
+                        {
+                            if (sub != null)
+                            {
+                                object busType = sub.GetValue("BusType");
+                                string busStr = busType != null ? busType.ToString() : string.Empty;
+                                if (busStr != string.Empty)
+                                {
+                                    yield return name;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static RegistryKey GetBaseKey()
+        {
+            return RegistryKey.OpenBaseKey(
+                RegistryHive.LocalMachine,
+                InternalCheckIsWow64() ? RegistryView.Registry64 : RegistryView.Registry32);
+        }
+        static bool is64BitProcess = (IntPtr.Size == 8);
+        static bool is64BitOperatingSystem = is64BitProcess || InternalCheckIsWow64();
+
+        [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWow64Process(
+            [In] IntPtr hProcess,
+            [Out] out bool wow64Process
+        );
+
+        public static bool InternalCheckIsWow64()
+        {
+            if ((Environment.OSVersion.Version.Major == 5 && Environment.OSVersion.Version.Minor >= 1) ||
+                Environment.OSVersion.Version.Major >= 6)
+            {
+                using (Process p = Process.GetCurrentProcess())
+                {
+                    bool retVal;
+                    if (!IsWow64Process(p.Handle, out retVal))
+                    {
+                        return false;
+                    }
+                    return retVal;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,140 @@
+﻿using MacIdChangeGPT.Busines;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Security.Principal;
+
+Yoneticizni();
+
+ChangeMacId();
+PhysicalAddress adres = GetMacAddress();
+//Console.ReadLine();
+
+void Yoneticizni()
+{
+    if (!Yoneticiznikontrol())
+    {
+        ProcessStartInfo program = new ProcessStartInfo();
+        program.UseShellExecute = true;
+        program.WorkingDirectory = Environment.CurrentDirectory;
+        program.FileName = Assembly.GetEntryAssembly().CodeBase;
+
+        program.Verb = "runas";
+
+        try
+        {
+            Process.Start(program);
+            Environment.Exit(0);
+        }
+        catch (Exception)
+        {
+            Debug.WriteLine("Program yönetici izni olmadan düzgün çalışmayacaktır!\n yinede çalıştırmak istiyor musunuz ? ");
+        }
+    }
+}
+
+bool Yoneticiznikontrol()
+{
+    WindowsIdentity id = WindowsIdentity.GetCurrent();
+    WindowsPrincipal principal = new WindowsPrincipal(id);
+
+    return principal.IsInRole(WindowsBuiltInRole.Administrator);
+}
+
+void ChangeMacId()
+{
+    NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+    PhysicalAddress[] addresses = StoreNetworkInterfaceAddresses();
+
+    PhysicalAddress afterPshcycalAddress = GetMacAddress();
+
+    //NetworkInterface nic = nics.FirstOrDefault(x => x.Name == "Wi-Fi" && x.OperationalStatus == OperationalStatus.Up);
+    //PhysicalAddress firstAddress = nic.GetPhysicalAddress();
+    string newMacAddress = Convert.ToHexString(GenerateRandomBytes(6));
+    bool isChangedMacId = Mac.SetMAC(newMacAddress);
+
+    if (isChangedMacId)
+    {
+        Console.WriteLine("MAC adresi başarıyla değiştirildi!");
+        Debug.WriteLine("Başarılı işlem. MAC adresi başarıyla değiştirildi!");
+    }
+    PhysicalAddress beforePshcycalAddress = GetMacAddress();
+}
+
+static byte[] GenerateRandomBytes(int length)
+{
+    byte[] randomBytes = new byte[length];
+
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        randomBytes[i] = (byte)random.Next(256);
+    }
+    return randomBytes;
+}
+
+static PhysicalAddress[] StoreNetworkInterfaceAddresses()
+{
+    IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+    NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+    if (nics == null || nics.Length < 1)
+    {
+        Console.WriteLine("  No network interfaces found.");
+        return null;
+    }
+
+    PhysicalAddress[] addresses = new PhysicalAddress[nics.Length];
+    int i = 0;
+    foreach (NetworkInterface adapter in nics)
+    {
+        IPInterfaceProperties properties = adapter.GetIPProperties();
+        PhysicalAddress address = adapter.GetPhysicalAddress();
+        byte[] bytes = address.GetAddressBytes();
+        PhysicalAddress newAddress = new(bytes);
+        addresses[i++] = newAddress;
+    }
+    return addresses;
+}
+
+//static string GetMacAddress()
+//{
+//    string macAddresses = "";
+
+//    foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+//    {
+//        if (nic.OperationalStatus == OperationalStatus.Up)
+//        {
+//            macAddresses += nic.GetPhysicalAddress().ToString();
+//            break;
+//        }
+//    }
+//    return macAddresses;
+//}
+//static string GetMacAddress()
+//{
+//    NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+//    String sMacAddress = string.Empty;
+//    foreach (NetworkInterface adapter in nics)
+//    {
+//        if (sMacAddress == String.Empty)// only return MAC Address from first card
+//        {
+//            IPInterfaceProperties properties = adapter.GetIPProperties();
+//            sMacAddress = adapter.GetPhysicalAddress().ToString();
+//        }
+//    }
+//    return sMacAddress;
+//}
+
+static PhysicalAddress GetMacAddress()
+{
+    foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+    {
+        // Only consider Ethernet network interfaces
+        if (nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 &&
+            nic.OperationalStatus == OperationalStatus.Up)
+        {
+            return nic.GetPhysicalAddress();
+        }
+    }
+    return null;
+}
